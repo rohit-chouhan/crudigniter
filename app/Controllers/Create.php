5d178432465f791b8484e22fdd54223e573dfc95
Rohit Chouhan <34239087+rohit-chouhan@users.noreply.github.com>
2021-09-11 13:08:53 +0530

    Update Create.php

--- app/Controllers/Create.php
+++ app/Controllers/Create.php
@@ -15,10 +15,27 @@ class Create extends BaseController
 	{
         $tables=$this->conn->query("SHOW TABLES FROM `".$this->conn->database."`  WHERE `Tables_in_".$this->conn->database."` LIKE '%users%'")->getResult();
         if($tables[0]->Tables_in_dbapi == $table) {
-            $request = \Config\Services::request();
-            $rawData = file_get_contents("php://input");
-            $records = json_decode($rawData,true);
-            $this->createData($records,$table);
+           $request = \Config\Services::request();
+           $rawData = file_get_contents("php://input");
+           $records = json_decode($rawData,true);
+
+           if(count($_GET)==0){
+              $this->createData($records,$table);
+           } else if(array_key_exists("form",$_GET)){
+
+              if(array_key_exists("image",$_GET)){
+                $images = explode(",",$_GET['image']);
+                for($i=0;$i<count($images);$i++){
+                  $file = $request->getFile($images[$i]);
+                  $file->store();
+                  $_POST[$images[$i]]=$file->getName();
+                }
+              }
+              
+              $this->createData($_POST,$table);
+
+           }
+
         } else {
             echo json_encode(array("status"=>false,"message"=>"Table not found"));
         }
@@ -26,6 +43,7 @@ class Create extends BaseController
     public function createData($arr,$table){
       $q = $this->conn->table($table);
       $data = $arr;
+
       if($q->insert($data)){
         echo json_encode(array("status"=>true,"message"=>"Records added successfully"));
       } else {
