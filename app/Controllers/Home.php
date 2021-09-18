abb7ff34bfe39862d22cf328fad77902283efde0
Rohit Chouhan <34239087+rohit-chouhan@users.noreply.github.com>
2021-09-18 19:48:04 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,21 +4,10 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-	// public $key;
-	// public function __construct(){
-	// 	$ok = 123;
-	// 	if($ok == 123){
-	// 		echo 'ok';
-	// 	} else {
-	// 		echo 'not ok';
-	// 	}
-	// }
-	public function index()
-	{
-		return view('welcome_message');
-	}
-
-	public function form(){
-		return view('form');
+	public function index(){
+		echo json_encode(array(
+			"status"=>true,
+			"message"=>"Read documentation here https://rohit-chouhan.github.io/crudigniter/"
+		));
 	}
 }
