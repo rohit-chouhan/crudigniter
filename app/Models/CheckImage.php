9bd32d71e4582f6d3b9b38c855a57e2a8f91dd36
Rohit Chouhan <34239087+rohit-chouhan@users.noreply.github.com>
2021-09-22 13:51:53 +0530

    Add files via upload

---
+++ app/Models/CheckImage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CheckImage extends Model
+{
+    public function checkImage($image){
+		$supported_image = array(
+			'gif',
+			'jpg',
+			'jpeg',
+			'png',
+			'bmp',
+			'pdf',
+			'zip',
+			'mp4',
+			'mp3'
+		);
+		
+		$src_file_name = $image;
+		$ext = strtolower(pathinfo($src_file_name, PATHINFO_EXTENSION)); 
+		if (in_array($ext, $supported_image)) {
+			return "http://media.".str_replace(['http://','https://'],"",base_url())."/".$image;
+		} else {
+			return $image;
+		}
+	}
+}
